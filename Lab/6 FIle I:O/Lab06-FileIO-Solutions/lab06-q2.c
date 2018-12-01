1e4ebc99c714ddaa2fa4cbbacd2fda868d46978a
Burson555 <by570@nyu.edu>
2018-12-01 17:32:52 +0800

    did lab 6
    as title

---
+++ Lab/6 FIle I:O/Lab06-FileIO-Solutions/lab06-q2.c
@@ -0,0 +1,52 @@
+#define _XOPEN_SOURCE 700
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/mman.h>
+#include <sys/wait.h>
+
+#define BUFSZ 1024
+
+int main(int argc, char** argv) {
+    
+    int fd1, fd2, or = 1;
+    struct stat info1, info2;
+    void* buffer = malloc(BUFSZ);
+    // this means the buffer content can be any data type
+    
+    fd1 = open(argv[1], O_RDWR, 0600);
+    if (fd1 == -1) {
+        perror("Pb à l'ouverture du fichier source");
+        exit(1);
+    }
+    fstat(fd1, &info1);
+    if (!(S_ISREG(info1.st_mode))) {
+        perror("Fichier source n'est pas régulier");
+        exit(1);
+    }
+    
+    if (stat(argv[2], &info2) != -1) { // check if the file exists
+        perror("Fichier cible existe");
+        exit(3);
+    }
+    fd2 = open(argv[2], O_RDWR|O_CREAT|O_TRUNC, 0600);
+    if (fd2 == -1) {
+        perror("Pb à l'ouverture du fichier cible");
+        exit(3);
+    }
+    
+    while(or > 0) {
+        or = read(fd1, buffer, BUFSZ);
+        write(fd2, buffer, or);
+    }
+    
+    close(fd1);
+    close(fd2);
+    
+    return 0;
+    
+}
